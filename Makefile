karlicoss/dunst

commit 678ea70c88a17162eea3268b37880b85f1d00f7b

Put user CFLAGS after the predefined flags
This allows the usage of the CFLAGS variable to override existing
parameters (e.g. optimizations) without editing config.mk.

config.mk
@@ -14,8 +14,8 @@ PKG_CONFIG ?= pkg-config
 
 # flags
 CPPFLAGS += -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
-CFLAGS   += -g --std=gnu99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC} ${CPPFLAGS}
-LDFLAGS  += -lm
+CFLAGS   := -g --std=gnu99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC} ${CPPFLAGS} ${CFLAGS}
+LDFLAGS  := -lm ${LDFLAGS}
 
 CPPFLAGS_DEBUG := -DDEBUG_BUILD
 CFLAGS_DEBUG   := -O0

commit 7f019a25c98449743963f2328f18b6c6eb14f994

Do not expand config.mk variables on assignment
Fix a bug introduced in 678ea70 where since the CFLAGS variable was
simply expanded it ignored the VERSION variable which was initialized
later.

Unfortunately there is no way to prepend a value to a recursively
expanded variable so instead we have to split the hardcoded compiler
flags into separate variables and combine them all later in the
makefile.

Fixes #500

Makefile
@@ -42,8 +42,8 @@ $(error "$(PKG_CONFIG) failed!")
 endif
 endif
 
-CFLAGS += -I. ${INCS}
-LDFLAGS+= -L. ${LIBS}
+CFLAGS  := ${DEFAULT_CPPFLAGS} ${CPPFLAGS} ${DEFAULT_CFLAGS} ${CFLAGS} -I. ${INCS}
+LDFLAGS := ${DEFAULT_LDFLAGS} ${LDFLAGS} -L. ${LIBS}
 
 SRC := $(sort $(shell find src/ -name '*.c'))
 OBJ := ${SRC:.c=.o}
@@ -53,7 +53,7 @@ TEST_OBJ := $(TEST_SRC:.c=.o)
 .PHONY: all debug
 all: doc dunst service
 
-debug: CFLAGS   += ${CFLAGS_DEBUG}
+debug: CFLAGS   += ${CPPFLAGS_DEBUG} ${CFLAGS_DEBUG}
 debug: LDFLAGS  += ${LDFLAGS_DEBUG}
 debug: CPPFLAGS += ${CPPFLAGS_DEBUG}
 debug: all

config.mk
@@ -13,9 +13,9 @@ PKG_CONFIG ?= pkg-config
 #STATIC= -DSTATIC_CONFIG # Warning: This is deprecated behavior
 
 # flags
-CPPFLAGS += -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
-CFLAGS   := -g --std=gnu99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC} ${CPPFLAGS} ${CFLAGS}
-LDFLAGS  := -lm ${LDFLAGS}
+DEFAULT_CPPFLAGS = -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
+DEFAULT_CFLAGS   = -g --std=gnu99 -pedantic -Wall -Wno-overlength-strings -Os ${STATIC}
+DEFAULT_LDFLAGS  = -lm
 
 CPPFLAGS_DEBUG := -DDEBUG_BUILD
 CFLAGS_DEBUG   := -O0